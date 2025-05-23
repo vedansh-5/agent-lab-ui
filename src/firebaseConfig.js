d7b3f4acc7aa7a434ed5c394960abbc70bc1ed10
rawkintrevo <trevor.d.grant@gmail.com>
2025-05-23 11:47:23 -0500

    move firebase config to uncomitted file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,17 +3,9 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions, httpsCallable } from "firebase/functions";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD4adujxJs1-uWL06t-pdhtWq4yL7Y2XPY",
-    authDomain: "agent-web-ui.firebaseapp.com",
-    projectId: "agent-web-ui",
-    storageBucket: "agent-web-ui.firebasestorage.app",
-    messagingSenderId: "292555495497",
-    appId: "1:292555495497:web:7f00d82dcc8da880b50798"
+import firebaseCredentials from './firebaseConfig.json';
 
-};
-
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseCredentials);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 const db = getFirestore(app);
